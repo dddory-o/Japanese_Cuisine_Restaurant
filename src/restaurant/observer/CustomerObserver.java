49e672bc92130cf731691de8bf9dbaa84b7ab25b
dddory_o <orynbassarovadinara@gmail.com>
2025-11-06 00:53:36 +0500

    observer

---
+++ src/restaurant/observer/CustomerObserver.java
@@ -0,0 +1,16 @@
+package restaurant.observer;
+
+import restaurant.factory.Meal;
+
+public class CustomerObserver implements Observer {
+    private String name;
+
+    public CustomerObserver(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String eventType, Meal meal) {
+        System.out.println("Customer " + name + " notified: Your " + meal.getName() + " is " + eventType + ".");
+    }
+}
